7b01ba47c6e6cdb6305c6130b458eee4832b3627
switch <3440501841@qq.com>
2024-08-28 23:00:07 +0800

    能成功么

--- shm/Shm.hpp
+++ shm/Shm.hpp
@@ -8,27 +8,32 @@
 #include<cstdio>
 #include<string>
 
-const std::string pathname = "/root/ice/pineapple/shm";
-const int proj_id = 0x49;
+const int gCreater = 1;
+const int gUser = 2;
 
-key_t GetCommKey(const std::string &pathname,int proj_id)
+class Shm
 {
-    key_t k = ftok(pathname.c_str(),proj_id);
+public:
+    Shm(const std::string &pathname, int proj_id)
+    :_pathname(pathname),_proj_id(proj_id)
+    {};
+    ~Shm();
+key_t GetCommKey()
+{
+    key_t k = ftok(_pathname.c_str(),_proj_id);
      if(k < 0)
      {
         perror("ftok");
      }
      return k;
 }
-
 //转十六进制捏
-std::string ToHex(key_t k)
+std::string ToHex()
 {
     char buffer[128];
-    snprintf(buffer,sizeof(buffer),"0x%x",k);
+    snprintf(buffer,sizeof(buffer),"0x%x",_key);
     return buffer;
 }
-
 //创建共享内存
 int ShmGet(key_t key,int size)
 {
@@ -39,5 +44,21 @@ int ShmGet(key_t key,int size)
     }
     return shmid;
 }
+private:
+    key_t _key;
+    int _shmid;
+    std::string _pathname;
+    int _proj_id;
+    int _who;
+};
+
+
+
+
+
+
+
+
+
 
 #endif 
