7d4ec9bf4fe7e51c44bef9b6fa9eaca41da0c166
switch <3440501841@qq.com>
2024-09-23 19:37:46 +0800

    荷叶饭漫展穿着高跟鞋走了两万步，牛逼

--- ticket/main.cc
+++ ticket/main.cc
@@ -3,56 +3,32 @@
 #include <unistd.h>
 #include <cstdio>
 #include "Thread.hpp"
+#include"LockGuard.hpp"
 
 using namespace ThreadMoudle;
 
 // 抢票要有全部的票数
 int tickets = 10000;
-pthread_mutex_t gmutex = PTHREAD_MUTEX_INITIALIZER;
 
 void route(ThreadData *td)
 {
     while (true)
     {
         // 加锁
-        pthread_mutex_lock(td->_lock);
+        LockGuard lockguard(td->_lock);
         if (tickets > 0)
         {
             // 抢票过程
             usleep(1000);                                             // 1ms -> 抢票花费的时间，这里暂时用这个休眠函数模拟
             printf("%s get ticket:%d\n", td->_name.c_str(), tickets); // 是谁没有抢到票
             tickets--;
-            pthread_mutex_unlock(td->_lock); // 解锁
         }
         else
         {
-            pthread_mutex_unlock(td->_lock); // 解锁
             break;
         }
     }
 }
-
-// void route(const std::string &name)
-// {
-//     while (true)
-//     {
-//         //加锁
-//         pthread_mutex_lock(&gmutex);
-//         if(tickets > 0)
-//         {
-//             //抢票过程
-//             usleep(1000);       // 1ms -> 抢票花费的时间，这里暂时用这个休眠函数模拟
-//             printf("%s get ticket:%d\n",name.c_str(),tickets);          //是谁没有抢到票
-//             tickets--;
-//             pthread_mutex_unlock(&gmutex);      //解锁
-//         }
-//         else
-//         {
-//             pthread_mutex_unlock(&gmutex);      //解锁
-//             break;
-//         }
-//     }
-// }
 
 static int threadnum = 4;
 
@@ -79,6 +55,80 @@ int main()
     pthread_mutex_destroy(&mutex);
     return 0;
 }
+
+// // 抢票要有全部的票数
+// int tickets = 10000;
+// pthread_mutex_t gmutex = PTHREAD_MUTEX_INITIALIZER;
+
+// void route(ThreadData *td)
+// {
+//     while (true)
+//     {
+//         // 加锁
+//         pthread_mutex_lock(td->_lock);
+//         if (tickets > 0)
+//         {
+//             // 抢票过程
+//             usleep(1000);                                             // 1ms -> 抢票花费的时间，这里暂时用这个休眠函数模拟
+//             printf("%s get ticket:%d\n", td->_name.c_str(), tickets); // 是谁没有抢到票
+//             tickets--;
+//             pthread_mutex_unlock(td->_lock); // 解锁
+//         }
+//         else
+//         {
+//             pthread_mutex_unlock(td->_lock); // 解锁
+//             break;
+//         }
+//     }
+// }
+
+// void route(const std::string &name)
+// {
+//     while (true)
+//     {
+//         //加锁
+//         pthread_mutex_lock(&gmutex);
+//         if(tickets > 0)
+//         {
+//             //抢票过程
+//             usleep(1000);       // 1ms -> 抢票花费的时间，这里暂时用这个休眠函数模拟
+//             printf("%s get ticket:%d\n",name.c_str(),tickets);          //是谁没有抢到票
+//             tickets--;
+//             pthread_mutex_unlock(&gmutex);      //解锁
+//         }
+//         else
+//         {
+//             pthread_mutex_unlock(&gmutex);      //解锁
+//             break;
+//         }
+//     }
+// }
+
+// // static int threadnum = 4;
+
+// // int main()
+// // {
+// //     pthread_mutex_t mutex;
+// //     pthread_mutex_init(&mutex, nullptr);
+
+// //     std::vector<Thread> threads;
+// //     for (int i = 0; i < threadnum; i++)
+// //     {
+// //         std::string name = "thread-" + std::to_string(i + 1);
+// //         ThreadData *td = new ThreadData(name, &mutex);
+// //         threads.emplace_back(name, route, td);
+// //     }
+// //     for (auto &thread : threads)
+// //     {
+// //         thread.Start();
+// //     }
+// //     for (auto &thread : threads)
+// //     {
+// //         thread.Join();
+// //     }
+// //     pthread_mutex_destroy(&mutex);
+// //     return 0;
+// // }
 
 // int main()
 // {
