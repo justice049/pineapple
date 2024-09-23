cf0537eccb8df7baff730632a136d93bb7016040
switch <3440501841@qq.com>
2024-09-23 20:56:33 +0800

    我补药去缅甸啊

---
+++ cond/test_cond.cc
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<string>
+#include<unistd.h>
+#include<pthread.h>
+
+const int num = 5;
+pthread_mutex_t gmutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t gcond = PTHREAD_COND_INITIALIZER;    //条件变量的初始化
+
+void* Wait(void* args)
+{
+    std::string name = static_cast<const char*>(args);
+    while (true)
+    {
+        pthread_mutex_lock(&gmutex);
+
+        pthread_cond_wait(&gcond,&gmutex);  
+        std::cout << "I am:" << name << std::endl;
+        
+        pthread_mutex_unlock(&gmutex);
+        sleep(1); 
+    }
+}
+
+int main()
+{
+    pthread_t threads[num];
+    for(int i=0;i<num;i++)
+    {
+        char *buffer=new char[1024];
+        snprintf(buffer,sizeof(buffer),"thread-%d",i+1);
+        pthread_create(threads+i,nullptr,Wait,(void*)buffer);
+    }
+
+    //唤醒这个线程
+    while (true)
+    {
+        pthread_cond_signal(&gcond);
+        std::cout << "唤醒一个线程..." << std::endl;
+        sleep(2);
+    }
+    
+    for(int i=0;i<num;i++)
+    {
+        pthread_join(threads[i],nullptr);
+    }
+    return 0;
+}
